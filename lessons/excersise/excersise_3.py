c185661041345cc4b61dd21c212ae4ef18e75443
Sreenivas-lakavath <83379336+Sreenivas-lakavath@users.noreply.github.com>
2025-10-11 19:55:46 -0400

    Day 1

--- lessons/excersise/excersise_3.py
+++ lessons/excersise/excersise_3.py
@@ -1,1 +1,19 @@
 # this is a python calculator
+operator = input ("Enter an operator (+ - )")
+num1 = float(input("Enter the first number"))
+num2 = float(input("Enter your second number"))
+
+if operator == "+":
+    result = num1+num2
+    print(result)
+elif operator == "-":
+    result = num1-num2
+    print(result)
+elif operator == "*":
+    result = num1*num2
+    print(result)
+elif operator == "/":
+    result = num1/num2
+    print(result)
+else:
+    print("please enter a valid operator")
